8ccad2b40f3efd0966b92843d70964b97a8757e0
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-07-28 12:06:17 +0600

    Proxy Gateway for post-core-api

---
+++ src/main/java/kz/dar/academy/backend/PostApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.dar.academy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostApiGatewayApplication.class, args);
+    }
+
+}
